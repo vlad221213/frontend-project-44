770f4b27c9264139efa297a2ff01e3a0f86c99cd
vlad221213 <vlad221213836@gmail.com>
2024-03-05 16:41:14 +0700

    error correction

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/extensions
 import readlineSync from 'readline-sync';
 import _ from 'lodash';
-export const askNameAndGreet = () => {
-    const name = readlineSync.question('May I have your name?');
-    console.log (`Hello, ${_.capitalize(name)}!`);
-}
+
+const askNameAndGreet = () => {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${_.capitalize(name)}!`);
+};
+export default askNameAndGreet;
