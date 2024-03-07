68a1b3f700417c4a0fd64a97236ff5c48d33dfa1
vlad221213 <vlad221213836@gmail.com>
2024-03-07 15:49:48 +0700

    error correction

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 // eslint-disable-next-line import/extensions
-import readlineSync from 'readline-sync';
-import _ from 'lodash';
+import brainEvenFunction from '../src/brain-even-function.js';
 
-console.log('Welcome to the Brain Games!');
-const name = _.capitalize(readlineSync.question('May I have your name?'));
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-for (let i = 0; i < 4; i += 1) {
-  if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-  } else {
-    const number = _.random(1, 99);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer:');
-    if ((number % 2 === 0 && answer === 'yes') || (number % 2 === 1 && answer === 'no')) {
-      console.log('Correct!');
-    } else if (number % 2 === 0) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`);
-      break;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
-      break;
-    }
-  }
-}
+brainEvenFunction();
