ce85137a7b6530a9312487455661f3222c0a6d23
vlad221213 <vlad221213836@gmail.com>
2024-03-07 14:20:38 +0700

    error correction

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import readlineSync from 'readline-sync';
 import _ from 'lodash';
 
 const askNameAndGreet = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${_.capitalize(name)}!`);
 };
