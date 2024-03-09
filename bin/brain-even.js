cd199ea453718a27e77bbf114e9c17418f61351a
vlad221213 <vlad221213836@gmail.com>
2024-03-09 16:48:05 +0700

    change import directory

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 // eslint-disable-next-line import/extensions
-import brainEvenFunction from '../src/brain-even-function.js';
+import brainEvenFunction from '../src/games/brain-even-function.js';
 
 brainEvenFunction();
