efa3173b9b94a7fe970c3dfde5d53ebf860a6d7d
vlad221213 <vlad221213836@gmail.com>
2024-03-09 16:48:36 +0700

    change import directory

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 // eslint-disable-next-line import/extensions
-import askNameAndGreet from '../src/cli.js';
+import askNameAndGreet from '../src/games/cli.js';
 
 askNameAndGreet();
