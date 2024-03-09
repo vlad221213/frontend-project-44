56db3f736f79a77aaaa02e124bc1ee1653d9ad69
vlad221213 <vlad221213836@gmail.com>
2024-03-09 18:16:46 +0700

    add brain-gsd

---
+++ bin/brain-gsd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+// eslint-disable-next-line import/extensions
+import brainGsdFunction from '../src/games/brain-gsd-function.js';
+
+brainGsdFunction();
