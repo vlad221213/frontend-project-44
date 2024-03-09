d496bbffdab1da2823501b36f3530e5a56007ebd
vlad221213 <vlad221213836@gmail.com>
2024-03-09 16:43:48 +0700

    architecture change

--- src/games/brain-even-function.js
+++ src/games/brain-even-function.js
@@ -1,30 +1,21 @@
 /* eslint-disable no-console */
-// eslint-disable-next-line import/extensions
-import readlineSync from 'readline-sync';
 import _ from 'lodash';
+// eslint-disable-next-line import/extensions
+import app from '../index.js';
 
-const brainEvenFunction = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = _.capitalize(readlineSync.question('May I have your name?'));
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 4; i += 1) {
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
+const brainEvenFunction = (howMuchRounds = 3) => {
+  let number = 0;
+  const question = [];
+  const expectAnswer = [];
+  for (let i = 0; i < howMuchRounds; i += 1) {
+    number = _.random(0, 99);
+    question.push(number);
+    if (number % 2 === 0) {
+      expectAnswer.push('yes');
     } else {
-      const number = _.random(1, 99);
-      console.log(`Question: ${number}`);
-      const answer = readlineSync.question('Your answer:');
-      if ((number % 2 === 0 && answer === 'yes') || (number % 2 === 1 && answer === 'no')) {
-        console.log('Correct!');
-      } else if (number % 2 === 0) {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`);
-        break;
-      } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
-        break;
-      }
+      expectAnswer.push('no');
     }
   }
+  app(question, expectAnswer, howMuchRounds);
 };
 export default brainEvenFunction;
