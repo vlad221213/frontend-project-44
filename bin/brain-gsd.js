21a4a2f4d30bf17ce2e157de6d99a5a5dfbf22b1
vlad221213 <vlad221213836@gmail.com>
2024-03-09 18:58:22 +0700

    rename brain-gsd.js to brain-gcd.js

--- bin/brain-gsd.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable no-console */
-// eslint-disable-next-line import/extensions
-import brainGsdFunction from '../src/games/brain-gsd-function.js';
-
-brainGsdFunction();
